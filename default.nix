6c197a0fef1b0573cfd5451ccde66760132df747
Galen Sprout <spro0640@mylaurier.ca>
2021-12-07 20:56:54 -0500

    cleaned up default.nix

--- default.nix
+++ default.nix
@@ -16,14 +16,6 @@
 }:
 let
   pkgs = import <nixpkgs> {};
-  # scrappyPkg = pkgs.fetchFromGitHub {
-    # owner = "Ace-Interview-Prep";
-    # repo = "scrappy";
-    # rev = "0.1.0.4";
-    # url = "https://github.com/Ace-Interview-Prep/scrappy/archive/refs/tags/0.1.0.4.tar.gz";
-    # sha256 = "0bv16mcz460h9spd8f522g223vip6br38wvymk9x5ycgk2zxw5g5";
-      # "17s66x4njr6dm8l32gcbcdf63rq9xxlhjlc7h3pnb6yysz7lfycr";
-  # };
 in
 with obelisk;
 project ./. ({ ... }: {
@@ -31,13 +23,6 @@ project ./. ({ ... }: {
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
-  # packages = scrappyPkg;
-  # overrides = haskellPackagesNew: haskellPackagesOld: rec {
-    # scrappy = nixpkgs.haskell.lib.dontCheck scrappy;
-  # };
-  # overrides = haskellPackagesNew: haskellPackagesOld: rec {
-    # scrappy = scrappy;
-  # };
   overrides =
     self: super:
     let
@@ -46,20 +31,10 @@ project ./. ({ ... }: {
         repo = "scrappy";
         rev = "0.1.0.5";
         sha256 = "11p673cl90nqmm4zqzzzpr7sy8dbbmgn8wqib1iqbvrs9kj0pbvg";
-          # "17s66x4njr6dm8l32gcbcdf63rq9xxlhjlc7h3pnb6yysz7lfycr";
       };
     in
       {
         scrappy = self.callCabal2nix "scrappy" scrappySrc {};
       };
-    
-  # overrides = haskellPackagesNew: hpOld: rec {
-    # scrappy = nixpkgs.haskell.lib.dontCheck (nixpkgs.fetchFromGitHub {
-      # owner = "Ace-Interview-Prep";
-      # repo = "scrappy";
-      # rev = "tag";
-      # sha256 = "17s66x4njr6dm8l32gcbcdf63rq9xxlhjlc7h3pnb6yysz7lfycr";
-    # });
-  # };
   withHoogle = true;
 })
